4873601b9dabec920ed587d5c5022f3c031cf68b
Alex Genovese <alex.genovese.italy@gmail.com>
2025-07-13 12:25:16 +0200

    add docs folder

---
+++ docs/assets/js/a7456010.540a7a41.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqq_x=self.webpackChunkqq_x||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ docs/assets/js/aba21aa0.0644cfd0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqq_x=self.webpackChunkqq_x||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
